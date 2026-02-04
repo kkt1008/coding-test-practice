534a54b95d484cc818ccd507a73a719405f8993e
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-04 22:20:34 +0900

    Create n번째 원소부터.cpp

---
+++ 프로그래머스 코딩 기초 트레이닝/n번째 원소부터.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+   
+    answer.insert(answer.end(),num_list.begin() + (n - 1) , num_list.end());
+    return answer;
+}
