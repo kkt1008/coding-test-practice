6a0b41bf682f12105b4413ff39d2c627aec9e144
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-04 23:20:19 +0900

    Add solution function to concatenate odd and even numbers

---
+++ 프로그래머스 코딩 기초 트레이닝/이어 붙인 수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    string temp1;
+    string temp2;
+    for(int i=0;i<num_list.size();i++){
+        if(num_list[i]%2 != 0){
+            temp1 += to_string(num_list[i]);
+        }
+        else{
+            temp2 += to_string(num_list[i]);
+        }
+    }
+    answer = stoi(temp1)+stoi(temp2);
+    
+    return answer;
+}
