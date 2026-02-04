224ac8cf887510faf4a24a7ec36fa928bdc436d1
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-04 22:38:18 +0900

    Create 문자열의 앞의 n글자.cpp

---
+++ 프로그래머스 코딩 기초 트레이닝/문자열의 앞의 n글자.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    answer.insert(answer.end(), my_string.begin(), my_string.begin()+n);
+    
+    return answer;
+}
