171107c173069ecee4270acd4e199b1209b9e42c
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-05 12:23:05 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1984 KB -BaekjoonHub

---
+++ C++14/백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,22 @@
+//2441
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num;
+	
+	for (int i = 0; i < num; i++)
+	{
+		for (int k = 0; k < i; k++) cout << " ";
+		
+		for (int j =0; j <num-i; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	
+	return 0;
+}
