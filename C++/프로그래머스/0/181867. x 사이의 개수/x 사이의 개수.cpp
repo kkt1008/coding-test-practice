b9275d7b6d6525889162be8bd51fb141c3e5ba25
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-07 20:31:48 +0900

    [level 0] Title: x 사이의 개수, Time: 4.99 ms, Memory: 7.25 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string myString) {
+    vector<int> answer;
+    int count=0;
+    for(char ch:myString){
+        if(ch != 'x'){
+            count++;    
+        }else{
+            answer.push_back(count);
+            count=0;
+        }
+    }
+    answer.push_back(count);
+    return answer;
+}
