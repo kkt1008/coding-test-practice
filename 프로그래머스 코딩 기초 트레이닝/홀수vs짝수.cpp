4380db231960db8a5d350063554ad3363c08d8ab
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-08 02:22:10 +0900

    Create 홀수vs짝수.cpp

---
+++ 프로그래머스 코딩 기초 트레이닝/홀수vs짝수.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int sum1=0; //짝수합
+    int sum2=0; //홀수합
+    
+    for(int i=0;i<num_list.size();i++){
+        if(i%2==0){
+            sum1+=num_list[i];        
+        }else{
+            sum2+=num_list[i];
+        }
+    }
+    if(sum1>sum2){
+        answer=sum1;
+    }else if(sum1<sum2){
+        answer=sum2;
+    }else{
+        answer=sum1;
+    }
+    return answer;
+}
