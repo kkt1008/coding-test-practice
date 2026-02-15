a583106916215d169f810e02b88aad61ccc2f622
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-15 16:32:27 +0900

    Create 조건 문자열.cpp

---
+++ 프로그래머스 코딩 기초 트레이닝/조건 문자열.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    int answer = 0;
+    
+    if((n>m) && ineq==">" && eq=="!"){
+        answer =1;
+    }else if((n<m) && ineq=="<" && eq=="!"){
+        answer =1;
+    }else if((n>=m) && ineq==">" && eq=="="){
+        answer =1;
+    }else if((n<=m) && ineq=="<" && eq=="="){
+        answer =1;
+    }else {
+        answer=0;
+    }
+    
+    return answer;
+}
