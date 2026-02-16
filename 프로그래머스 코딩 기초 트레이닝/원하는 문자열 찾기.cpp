b0f1a0990ef2bf5db0591d59f5ac5197de8a4606
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-16 22:33:01 +0900

    Create 원하는 문자열 찾기.cpp

---
+++ 프로그래머스 코딩 기초 트레이닝/원하는 문자열 찾기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    
+    string tempMyString;
+    string tempPat;
+    
+    for(char &ch:myString){
+        tempMyString+=tolower(ch);
+    }
+    for(char &ch:pat){
+        tempPat+=tolower(ch);
+    }
+    
+    if(myString.size() < pat.size()){//pat이 길면 무조건 0
+        answer=0;
+    }else{//pat이 같거나 짧을때 문자열 비교
+        if(tempMyString.find(tempPat) != string::npos){
+            answer=1;
+        }else{
+            answer=0;
+        }    
+    }
+    
+    return answer;
+}
