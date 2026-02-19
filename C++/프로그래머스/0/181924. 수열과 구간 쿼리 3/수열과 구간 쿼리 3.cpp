7a9acb57627993e69b48b3cbdbc13679b1928861
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-20 00:17:35 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.21 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <utility>
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    for(auto &q :queries){
+        int i=q[0];
+        int j=q[1];
+        swap(arr[i],arr[j]);
+    }   
+    return arr;
+}
