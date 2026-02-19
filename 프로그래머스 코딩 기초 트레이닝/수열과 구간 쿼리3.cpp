0ad2214364ed89ee570748f55ef93548f967f292
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-20 00:18:49 +0900

    Create 수열과 구간 쿼리3.cpp
    #include <utility> 라이브러리의 swap함수 사용 예제. 
    vector의 요소값 위치를 서로 교환한다

---
+++ 프로그래머스 코딩 기초 트레이닝/수열과 구간 쿼리3.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <utility>
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    for(auto &q :queries){
+        int i=q[0];
+        int j=q[1];
+        swap(arr[i],arr[j]);
+    }   
+    return arr;
+}
