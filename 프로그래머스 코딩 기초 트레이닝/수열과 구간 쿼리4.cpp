a1d92f7bc96a376028087a673c16b8c5343bac19
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-25 17:49:18 +0900

    Create 수열과 구간 쿼리4.cpp

---
+++ 프로그래머스 코딩 기초 트레이닝/수열과 구간 쿼리4.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    for(int i=0;i<queries.size();i++){
+        int s = queries[i][0];
+        int e = queries[i][1];
+        int k = queries[i][2]; 
+        
+        for(int i = s;i <= e;i++){
+            if(i % k == 0){//i가 k의 배수이면 arr[i]+1
+                arr[i]+=1;
+            }
+        }
+    }
+    answer=arr;
+    return answer;
+}
