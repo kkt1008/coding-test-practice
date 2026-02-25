ded79530510db2063417fa81dce6d972b6f1962a
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-25 17:11:59 +0900

    Create 수열과 구간 쿼리2.cpp
    2차원 vector자료형
    int타입이 표현 가능한 최댓값 INT_MAX 사용을 통한 <climits>라이브러리 사용

---
+++ 프로그래머스 코딩 기초 트레이닝/수열과 구간 쿼리2.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <climits> // INT_MAX 사용하기 위해 추가
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer;
+    
+    for(int i=0;i<queries.size();i++){//쿼리의 갯수만큼 반복
+        int s = queries[i][0];
+        int e = queries[i][1];
+        int k = queries[i][2]; 
+        int temp = INT_MAX;
+            
+        for(int i=s;i<=e;i++){
+            if(arr[i] > k){
+                temp = min(temp,arr[i]);
+            }
+        }
+        
+        if(temp == INT_MAX){
+            answer.push_back(-1);
+        }else{
+            answer.push_back(temp);
+        }
+    }    
+    return answer;
+}
