606065736be00864f017ec44a7fba571b2a3728e
kkt1008 <94011501+kkt1008@users.noreply.github.com>
2026-02-26 22:30:31 +0900

    [level 0] Title: 배열 만들기 4, Time: 1.29 ms, Memory: 6.48 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    int i = 0;
+    // i가 arr의 길이보다 작으면 다음 작업을 반복한다
+    //stk가 빈 배열이면 arr[i]를 stk에 추가하고 i+=1 
+    //stk에 원소가 있고, stk의 마지막 원소가 arr[i]보다 작으면 arr[i]를 stk의 뒤에 추가하고 i+=1
+    //stk에 원소가 있는데 stk의 마지막 원소가 arr[i]보다 크거나 같으면 stk의 마지막 원소를 stk에서 제거
+    
+    while(i<arr.size()){// i가 arr의 길이보다 작으면 다음 작업을 반복
+        if(stk.empty()){// stk가 빈 배열이면 arr[i]를 stk에 추가하고 i++
+            stk.push_back(arr[i]);
+            i++;
+        }else if(stk.back() < arr[i]){//stk에 원소가 있고, stk의 마지막 원소가 arr[i]보다 작으면 arr[i]를 stk의 뒤에 추가하고 i++
+            stk.push_back(arr[i]);
+            i++;
+        }else if(stk.back() >= arr[i]){//stk에 원소가 있는데 stk의 마지막 원소가 arr[i]보다 크거나 같으면 stk의 마지막 원소를 stk에서 제거
+            stk.pop_back();
+        }
+    }
+    return stk;
+}
